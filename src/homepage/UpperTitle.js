a1895914526f1a97e35f93e369ac5ea8ca535142
Nikhil <97503662+nikhilhvr@users.noreply.github.com>
2022-06-26 21:34:58 +0530

    main

---
+++ src/homepage/UpperTitle.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../Components/UpperTitle.css'
+
+
+function UpperTitle() {
+  return (
+    <div className="UpperTitle">
+      <p className='title'>Strom In a Coffeepot</p>
+      <p className='sub-title'>cafe in Munich
+      <p> Open upto 5:00PM</p>
+      </p>
+      </div>
+
+      
+      
+      
+
+
+      
+  )
+}
+
+export default UpperTitle

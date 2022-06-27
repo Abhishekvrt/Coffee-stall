44f447cf7fec0a35534af4a7dc5037a3b5856dd6
Abhishek-Kumar <abhishekphd14@gmail.com>
2022-06-27 19:17:48 +0530

    main

--- src/Home.js
+++ src/Home.js
@@ -4,17 +4,19 @@ import './App.css';
 import './Components/maps.css'
 import UpperTitle from './homepage/UpperTitle'
 import Maps from './homepage/Maps';
+// import Footer from './homepage/Footer'
 
 function Home() {
   return (
     <>
     <div className="Home">
       <UpperTitle/>
+      <Maps/>
+       
+
       
     </div>
-    {/* <div className='Maps'>
-    <Maps/>
-    </div> */}
+     
     
     
   
--- src/homepage/Maps.js
+++ src/homepage/Maps.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import '../Components/maps.css'
+
+
 export default function Maps() {
   return (
     <div class='container'>
         <h1>Location</h1>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2663.4369838684643!2d11.5607772!3d48.1210957!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479e75eb67b769a7%3A0x75bed7038a1fa3ca!2sStorm%20in%20a%20Coffeepot!5e0!3m2!1sen!2sin!4v1656258443767!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <img src="" alt="" />
+        <h4>is here</h4>
     </div>
 
   )
